ff558cdaba62bd2e2e0cf29119b9ce5544f8946a
phdljr <phdljr@naver.com>
2023-07-13 17:22:05 +0900

    Initial commit

---
+++ src/test/java/kr/ac/phdljr/sbaws/SbawsApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.ac.phdljr.sbaws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbawsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
