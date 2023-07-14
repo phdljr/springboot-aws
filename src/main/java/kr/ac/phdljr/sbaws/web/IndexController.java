b4f8168884d8481c6b6afa47abb1421e6c722b71
phdljr <phdljr@naver.com>
2023-07-15 04:10:28 +0900

    feat: security, OAuth2

--- src/main/java/kr/ac/phdljr/sbaws/web/IndexController.java
+++ src/main/java/kr/ac/phdljr/sbaws/web/IndexController.java
@@ -1,5 +1,7 @@
 package kr.ac.phdljr.sbaws.web;
 
+import jakarta.servlet.http.HttpSession;
+import kr.ac.phdljr.sbaws.config.auth.dto.SessionUser;
 import kr.ac.phdljr.sbaws.service.posts.PostsService;
 import kr.ac.phdljr.sbaws.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -13,10 +15,16 @@ import org.springframework.web.bind.annotation.PathVariable;
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
     public String index(Model model){
         model.addAttribute("posts", postsService.findAllDesc());
+
+        SessionUser user = (SessionUser)httpSession.getAttribute("user");
+        if(user != null){
+            model.addAttribute("userName", user.getName());
+        }
         return "index";
     }
 
