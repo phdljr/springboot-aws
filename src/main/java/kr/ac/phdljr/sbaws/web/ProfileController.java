d0911be9f9e5544a28b2ea03925f082af89d4dc2
phdljr <phdljr@naver.com>
2023-07-17 21:43:25 +0900

    feat: add profile api

---
+++ src/main/java/kr/ac/phdljr/sbaws/web/ProfileController.java
@@ -0,0 +1,28 @@
+package kr.ac.phdljr.sbaws.web;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
+public class ProfileController {
+    private final Environment env;
+
+    @GetMapping("/profile")
+    public String profile(){
+        // 현재 실행 중인 프로파일 불러오기
+        List<String> profiles = Arrays.asList(env.getActiveProfiles());
+        List<String> realProfiles = Arrays.asList("real", "real1", "real2");
+        String defaultProfile = profiles.isEmpty() ? "default" : profiles.get(0);
+
+        return profiles.stream()
+                .filter(realProfiles::contains)
+                .findAny()
+                .orElse(defaultProfile);
+    }
+}
